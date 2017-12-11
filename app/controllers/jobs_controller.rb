450b8e8335dae9f8fde36582d6aff6be1dd0babd
Kelley Jenkins <ckjenkins3@gmail.com>
2017-12-11 16:29:13 -0700

    adds update job functionality

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,5 @@
 class JobsController < ApplicationController
+  before_action :set_job, only: [:show, :destroy, :edit, :update]
   def index
     @company = Company.find(params[:company_id])
     @jobs = @company.jobs
@@ -21,15 +22,16 @@ class JobsController < ApplicationController
   end
 
   def show
-    @job = Job.find(params[:id])
   end
 
   def edit
-    # implement on your own!
+    @company = Company.find(params[:company_id])
   end
 
   def update
-    # implement on your own!
+    @job.update(job_params)
+
+    redirect_to company_job_path(@job.company, @job)
   end
 
   def destroy
@@ -40,5 +42,9 @@ class JobsController < ApplicationController
 
   def job_params
     params.require(:job).permit(:title, :description, :level_of_interest, :city)
+  end
+
+  def set_job
+    @job = Job.find(params[:id])
   end
 end
---
+++ spec/features/jobs/user_edits_existing_job_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "User edits an existing job" do
+  scenario "a user can edit a job" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", description: "So fun!", level_of_interest: 70, city: "Denver")
+
+    visit edit_company_job_path(company, job)
+
+    fill_in "job[title]", with: "Software Engineer"
+    click_button "Update"
+
+    expect(current_path).to eq(company_job_path(company, job))
+    expect(page).to have_content("Software Engineer")
+    expect(page).to_not have_content("Developer")
+  end
+end
