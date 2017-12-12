6f193fbb7414807bda63c2584d914d50ec690873
Kelley Jenkins <ckjenkins3@gmail.com>
2017-12-12 13:51:09 -0700

    add factory_bot files and config

---
+++ spec/factories/categories.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :category do
+    title "Computers"
+  end
+end
---
+++ spec/factories/jobs.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :job do
+    title "Web Designer"
+    description "Practically perfect in every way"
+    level_of_interest 90
+    city "Denver"
+    cateogory
+  end
+end
