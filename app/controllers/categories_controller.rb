29055f3321ca20830a230bc96b9dd7f5e393f40f
Kelley Jenkins <ckjenkins3@gmail.com>
2017-12-11 17:47:17 -0700

    edit category name to category title

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,26 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @categories = Category.all
+  end
+
   def new
-  end 
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "#{@category.title} added!"
+      redirect_to category_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title)
+  end
 
 end
