01efad6d87de98b6fa821f099a265aa706bea9f1
Kelley Jenkins <ckjenkins3@gmail.com>
2017-12-12 17:16:08 -0700

    still working on factorybot

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :job do
     title "Job"
-    description "Practically perfect in every way"
+    sequence description "Practically perfect in every way"
     level_of_interest 90
     city "Denver"
+    company
     category
   end
 end
--- spec/features/categories/user_sees_all_jobs_in_category_spec.rb
+++ spec/features/categories/user_sees_all_jobs_in_category_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 describe "User sees one category" do
   scenario "a user sees all jobs in category" do
-    category = create(:category)
     company = create(:company)
-    job = company.jobs.create!(title: "Developer", description: "So fun!", level_of_interest: 70, city: "Denver", category_id: category.id)
+    category = create(:category)
+    job1 = company.jobs.create!(title: "Developer", description: "So fun!", level_of_interest: 70, city: "Denver", category_id: category.id)
     job2 = company.jobs.create!(title: "Designer", description: "Pretty!", level_of_interest: 65, city: "Chicago", category_id: category.id)
 
 
     visit category_path(category)
 
     expect(current_path).to eq(category_path(category))
-    expect(page).to have_content("Developer")
-    expect(page).to have_content("So fun!")
-    expect(page).to have_content("Designer")
+    expect(page).to have_content(job1.title)
+    expect(page).to have_content(job1.description)
+    expect(page).to have_content(job2.title)
   end
 end
